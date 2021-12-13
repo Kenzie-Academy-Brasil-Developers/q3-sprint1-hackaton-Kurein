3ca3c73a2cfe031cb872942c4610b05407d4742d
Kaueh <kaueh1234@gmail.com>
2021-12-13 17:36:17 -0300

    entrega corrigida

--- hackaton.py
+++ hackaton.py
@@ -1,7 +1,9 @@
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
 def get_score(team_name, teams):
     for index, team in enumerate(teams):
         if team == team_name:
             a = index + 1
-        
-
     return f"A {team_name} ficou classificada em {a}"
